7bab5306b37336eb24796d2b3a09a5f7aaecca47
wangxingzhuo <wangxingzhuo@zuoyebang.com>
2022-07-26 17:21:28 +0800

    base64 decode

--- main.go
+++ main.go
@@ -1,39 +1,47 @@
 package goutils
 
 import (
-    "crypto/md5"
-    "encoding/base64"
-    "fmt"
-    "time"
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"time"
 )
 
 func EncodeBase64(msg string) string {
-    return base64.StdEncoding.EncodeToString([]byte(msg))
+	return base64.StdEncoding.EncodeToString([]byte(msg))
+}
+
+func DecodeBase64(msg string) (string, error) {
+	ret, err := base64.StdEncoding.DecodeString(msg)
+	if nil == err {
+		return string(ret), nil
+	}
+	return "", err
 }
 
 func MD5(src string) string {
-    return fmt.Sprintf("%x", md5.Sum([]byte(src)))
+	return fmt.Sprintf("%x", md5.Sum([]byte(src)))
 }
 
 func NowNano() uint64 {
-    return uint64(time.Now().UnixNano())
+	return uint64(time.Now().UnixNano())
 }
 
 func Now() int64 {
-    return time.Now().Unix()
+	return time.Now().Unix()
 }
 
 func random(foo uint64) float64 {
-    return float64(( foo * 9301 + 49297 ) % 233280) / 233280
+	return float64((foo*9301+49297)%233280) / 233280
 }
 
 func Random() float64 {
-    now := NowNano()
-    now = uint64(random(now) * (1 << 63)) + NowNano()
-    return float64(( now * 9301 + 49297 ) % 233280) / 233280
+	now := NowNano()
+	now = uint64(random(now)*(1<<63)) + NowNano()
+	return float64((now*9301+49297)%233280) / 233280
 }
 
 func RandomString(length int) string {
-    foo := fmt.Sprintf("%x", int64(Random() * 10000000))
-    return EncodeBase64(foo)
+	foo := fmt.Sprintf("%x", int64(Random()*10000000))
+	return EncodeBase64(foo)
 }
